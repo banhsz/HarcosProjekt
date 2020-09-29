a5d2afc3a5130ea4ad3c23c3973a00672e7aa743
Petrik Lajos Szg <info@petrik.hu>
2020-09-29 11:40:54 +0200

    Getterek, setterek beállítása

--- HarcosProjekt/Harcos.cs
+++ HarcosProjekt/Harcos.cs
@@ -8,11 +8,31 @@ namespace HarcosProjekt
 {
     class Harcos
     {
+        //adattagok
         string nev;
         int szint;
         int tapasztalat;
         int eletero;
         int alapEletero;
         int alapSebzes;
+
+        //konstruktor
+        public Harcos(string nev,int statuszSablon)
+        {
+
+        }
+
+        //get-setterek
+        public string Nev { get => nev; set => nev = value; }
+        public int Szint { get => szint; set => szint = value; }
+        public int Tapasztalat { get => tapasztalat; set => tapasztalat = value; }
+        public int AlapEletero { get => alapEletero; }
+        public int AlapSebzes { get => alapSebzes; }
+        public int Eletero { get => eletero; set => eletero = value; }
+        public int Sebzes { get => alapSebzes+szint; }
+        public int SzintLepeshez { get => szint*5+10; }
+        public int MaxEletero { get => alapEletero + szint*3; }
+
+        //metódusok
     }
 }
